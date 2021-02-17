f79d9ed12afc6ba749f96ed1ae17a9440b967869
victorandruscabit <victor.v.andrusca@gmail.com>
2021-02-17 14:46:41 +0200

    addd clas person

---
+++ src/main/java/com/andrusca/gitoverview/Person.java
@@ -0,0 +1,32 @@
+package com.andrusca.gitoverview;
+
+public class Person extends BaseEntiy{
+    private String firstName;
+
+    private String lastName;
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + super.getId() + '\'' +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
